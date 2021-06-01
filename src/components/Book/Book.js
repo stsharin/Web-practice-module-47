ac5483e6445a7acbf7dfc1702cb5ff43171008ef
Saraban Tasnim Sharin <stssharin@gmail.com>
2021-06-02 03:18:19 +0600

    edited dates

---
+++ src/components/Book/Book.js
@@ -0,0 +1,65 @@
+import React, { useContext, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { UserContext } from '../../App';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+    MuiPickersUtilsProvider,
+    KeyboardDatePicker,
+} from '@material-ui/pickers';
+import { Button } from '@material-ui/core';
+
+const Book = () => {
+    const { bedType } = useParams();
+    const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const [selectedDate, setSelectedDate] = useState({
+        checkIn: new Date(),
+        checkOut: new Date()
+    });
+
+    const handleCheckInDate = (date) => {
+        setSelectedDate(date);
+    };
+    const handleCheckOutDate = (date) => {
+        setSelectedDate(date);
+    };
+    
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Hello, {loggedInUser.name}! Let's book a {bedType} Room.</h1>
+            <p>Want a <Link to="/home">different room?</Link> </p>
+
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                <Grid container justify="space-around">
+                    <KeyboardDatePicker
+                        disableToolbar
+                        variant="inline"
+                        format="MM/dd/yyyy"
+                        margin="normal"
+                        id="date-picker-inline"
+                        label="Check In Date"
+                        value={selectedDate.checkIn}
+                        onChange={handleCheckInDate}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
+                    />
+                    <KeyboardDatePicker
+                        margin="normal"
+                        id="date-picker-dialog"
+                        label="Check Out Date"
+                        format="MM/dd/yyyy"
+                        value={selectedDate.checkOut}
+                        onChange={handleCheckOutDate}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
+                    />
+                </Grid>
+                <Button variant="contained" color="primary">Book Now</Button>
+            </MuiPickersUtilsProvider>
+        </div>
+    );
+};
+
+export default Book;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCXTvP4Cq_0OH-d69AJQlxgh2sqMEkI74g",
+  authDomain: "burj-al-arab-22962.firebaseapp.com",
+  projectId: "burj-al-arab-22962",
+  storageBucket: "burj-al-arab-22962.appspot.com",
+  messagingSenderId: "805364343156",
+  appId: "1:805364343156:web:6c2eedb6142b206bf30c21"
+};
+  export default firebaseConfig;
